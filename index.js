70dd5a29175e54dda76c1e1978798bcc11cbc426
LukashevychAndriy <andriy.lukashewich@gmail.com>
2021-10-06 19:20:12 +0300

    init commit

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const xp = +prompt('Введіть х початкове'); // x початкове
-const xk = +prompt('Введіть х кінцеве'); // x кінцеве
-const dx = +prompt('Введіть крок'); // крок обчислень
+const a = +prompt('Введіть значення параметру а'); // Запрошуємо ввід параметру а
+const b = +prompt('Введіть значення параметру b'); // Запрошуємо ввід параметру b
+const c = +prompt('Введіть значення параметру c'); // Запрошуємо ввід параметру c
+const xp = +prompt('Введіть х початкове'); // Запрошуємо ввід x початкового
+const xk = +prompt('Введіть х кінцеве'); // Запрошуємо ввід х кінцевого
+const dx = +prompt('Введіть крок'); // Запрошуємо ввід кроку
 
 console.log('---------------------------');
-console.log('|\tx\t|\ty\t|');
+console.log('|\tx\t|\tF\t|');
 console.log('---------------------------');
 
 for (let x = xp; x <= xk; x += dx) {
-  const A = (2 + x) / x ** 2 + 1;
+  let F;
 
-  let B;
-
-  if (x < 0) {
-    B = x ** 3 - 2 * x ** 4;
-  } else if (x >= 0 && x <= 2) {
-    B = (Math.abs(x) + Math.E ** x) ** 3;
+  if (x < 0.6 && b + c !== 0) {
+    F = a * x ** 2 + b ** 2 + c;
+  } else if (x > 0.6 && b + c === 0) {
+    F = (x - a) / (x - c);
   } else {
-    B = 4 * Math.cos(x ** 2 - 2);
+    F = x / c + x / a;
   }
 
-  const y = A + B;
-
-  console.log(`|\t${x.toFixed(2)}\t|\t${y.toFixed(3)}\t|`);
+  console.log(`|\t${x.toFixed(2)}\t|\t${F.toFixed(3)}\t|`);
 }
