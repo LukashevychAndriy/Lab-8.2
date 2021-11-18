6367ef7d928f7ef66191690688a451070396033b
LukashevychAndriy <andriy.lukashewich@gmail.com>
2021-11-18 04:23:59 +0200

    init commit

--- index.js
+++ index.js
@@ -1,94 +1,96 @@
-function genArr(min, max, amount) {
+function genArr(min, max, rows, cols) {
   const arr = [];
 
-  for (let i = 1; i <= amount; i++) {
-    const n = min + Math.random() * (max - min);
-
-    arr.push(n);
+  for (let i = 0; i < rows; i++) {
+    arr[i] = [];
+    for (let k = 0; k < cols; k++) {
+      arr[i][k] = Math.floor(min + Math.random() * (max - min + 1));
+    }
   }
 
   return arr;
 }
 
-function getMaxAbsIdx(arr) {
-  let maxAbs = Math.abs(arr[0]);
-  let maxAbsIdx = 0;
-
-  for (let i = 1; i < arr.length; i++) {
-    if (Math.abs(arr[i]) > maxAbs) {
-      maxAbs = Math.abs(arr[i]);
-      maxAbsIdx = i;
+function sort(arr, rowCount, colCount) {
+  for (let i = 0; i < colCount - 1; i++) {
+    for (let k = colCount - 2; k >= i; k--) {
+      if (arr[0][k] < arr[0][k + 1] ||
+          arr[0][k] === arr[0][k + 1] && arr[1][k] < arr[1][k + 1] ||
+          arr[0][k] === arr[0][k + 1] && arr[1][k] === arr[1][k + 1] && arr[2][k] > arr[2][k + 1]) {
+        switchColumns(arr, rowCount, k, k + 1);
+      }
     }
   }
+}
 
-  return maxAbsIdx;
+function switchColumns(arr, rowCount, col1Idx, col2Idx) {
+  for (let i = 0; i <= rowCount - 1; i++) {
+    const tmp = arr[i][col1Idx];
+    arr[i][col1Idx] = arr[i][col2Idx];
+    arr[i][col2Idx] = tmp;
+  }
 }
 
 function getSum(arr) {
-  let positiveIdx = -1;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      positiveIdx = i;
-      break;
-    }
-  }
-
   let sum = 0;
 
-  if (positiveIdx === -1) {
-    return sum;
-  }
-
-  for (let i = positiveIdx + 1; i < arr.length; i++) {
-    sum += arr[i];
+  for (let i = 0; i < arr.length; i++) {
+    for (let k = 0; k < arr[i].length; k++) {
+      if (arr[i][k] % 3 === 0 && arr[i][k] > 0) {
+        sum += arr[i][k];
+      }
+    }
   }
 
   return sum;
 }
 
-function sortArray(arr, a, b) {
-  let arr1 = [];
-  let arr2 = [];
+function getAmount(arr) {
+  let amount = 0;
 
   for (let i = 0; i < arr.length; i++) {
-    if (+arr[i].toFixed(0) >= a && +arr[i].toFixed(0) <= b) {
-      arr1.push(arr[i]);
-    } else {
-      arr2.push(arr[i]);
+    for (let k = 0; k < arr[i].length; k++) {
+      if (arr[i][k] % 3 === 0 && arr[i][k] > 0) {
+        amount++;
+        setZero(arr, i, k);
+      }
     }
   }
 
-  let k = 0;
+  return amount
+}
 
-  for (let i = 0; i < arr1.length; i++) {
-    arr[k] = arr1[i];
-    k++;
-  }
-
-  for (let i = 0; i < arr2.length; i++) {
-    arr[k] = arr2[i];
-    k++;
-  }
+function setZero(arr, rowIdx, colIdx) {
+  arr[rowIdx][colIdx] = 0;
 }
 
 function arrToString(arr) {
   let string = '';
 
   for (let i = 0; i < arr.length; i++) {
-    string += `${i}:\t${arr[i]}\n`;
+    for (let k = 0; k < arr[i].length; k++) {
+      if (arr[i][k] >= 0) {
+        string += ' ';
+      }
+
+      if (arr[i][k] < 10  && arr[i][k] > -10) {
+        string += ' ';
+      }
+
+      string += arr[i][k] + '\t';
+    }
+
+    string += '\n';
   }
 
-  return string;
+  return string.slice(1);
 }
 
-const n = +prompt('Введіть значення параметру n');
-const a = +prompt('Введіть значення параметру a');
-const b = +prompt('Введіть значення параметру b');
-const arr = genArr(-100, 100, n);
+const arr = genArr(-21, 24, 9, 6);
 
-console.log('Початковий масив:\n', arrToString(arr));
-console.log('Індекс елементу модуль якого найбільший:', getMaxAbsIdx(arr));
-console.log('Сума елементів після першого додатнього елементу:', getSum(arr));
-sortArray(arr, a, b);
-console.log('Кінцевий масив:\n', arrToString(arr));
+console.log('Початкова матриця:\n', arrToString(arr));
+sort(arr, 9, 6);
+console.log('Посортована матриця:\n', arrToString(arr));
+console.log('Сума:\n', getSum(arr));
+console.log('Кількість:\n', getAmount(arr));
+console.log('Кінцева матриця:\n', arrToString(arr));
